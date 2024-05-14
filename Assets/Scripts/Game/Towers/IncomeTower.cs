81b66ba714635911bcb2cb7ff8ebbf683b7d86cf
Entiq228 <pavlodigalevic@gmail.com>
2024-05-14 12:37:05 +0300

    3.1

--- Assets/Scripts/Game/Towers/IncomeTower.cs
+++ Assets/Scripts/Game/Towers/IncomeTower.cs
@@ -3,36 +3,26 @@ using UnityEngine;
 
 public class IncomeTower : Tower
 {
-    //FIELDS
-    //Income value
     public int incomeValue;
-    //Interval for income
     public float interval;
-    //Coin image object
     public GameObject obj_coin;
 
-    //METHODS
-    //Init
     void Start()
     {
         StartCoroutine(Interval());
     }
-    //Interval IEnumerator
     IEnumerator Interval()
     {
         yield return new WaitForSeconds(interval);
-        //Trigger the income increase
         IncreaseIncome();
 
         StartCoroutine(Interval());
     }
-    //Trigger Income Increase
     public void IncreaseIncome()
     {
         GameManager.instance.currency.Gain(incomeValue);
         StartCoroutine(CoinIndication());
     }
-    //Show coin indication over the tower for short time (0.5 second)
     IEnumerator CoinIndication()
     {
         obj_coin.SetActive(true);
--- Assets/Scripts/Game/Towers/LazerTower.cs
+++ Assets/Scripts/Game/Towers/LazerTower.cs
@@ -3,35 +3,23 @@ using UnityEngine;
 
 public class LazerTower : Tower
 {
-    //FIELDS
-    //Damage
     public int damage;
-    //Prefab (Shooting Item)
     public GameObject prefab_shootItem;
-    //Shoot interval
     public float interval;
 
-    //METHODS
-    //Init (Start the shooting interval)
     void Start()
     {
-        //Start the shooting interval
         StartCoroutine(ShootDeleay());
     }
-    //Interval for shooting
     IEnumerator ShootDeleay()
     {
         yield return new WaitForSeconds(interval);
-        //Shoot item method
         ShootItem();
         StartCoroutine(ShootDeleay());
     }
-    //Shoot item
     void ShootItem()
     {
-        //Instantiate shoot item
         GameObject shotItem = Instantiate(prefab_shootItem, transform);
-        //Set its values
         shotItem.GetComponent<ShootItem>().Init(damage);
     }
 }
